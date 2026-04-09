81ba6833ac7a36f30c5e6660ad4e7f99363fce34
Nicolas Malvano <malvanonicolas@gmail.com>
2026-04-09 18:19:05 -0300

    Feat/file-upload flow (module, controller, service, repository)

---
+++ gastroflow/src/file-upload/file-upload.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "../users/entities/user.entity";
+import { Repository } from "typeorm";
+import { FileUploadRepository } from "./file-upload.reposiroty";
+
+@Injectable()
+export class FileUploadService {
+  constructor(
+    private readonly fileUploadRepository: FileUploadRepository,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+  async uploadFile(userId: string, file: Express.Multer.File): Promise<User>{
+
+    const foundUser = await this.userRepository.findOneBy({id: userId});
+    if(!foundUser) throw new NotFoundException(`Producto con id: ${userId} no encontrado`)
+
+    const response = await this.fileUploadRepository.uploadImage(file);
+
+    foundUser.imgUrl = response.secure_url;
+    await this.userRepository.save(foundUser);
+    
+    const updatedUser = await this.userRepository.findOneBy({id: userId});
+    if(!updatedUser) throw NotFoundException;
+    return updatedUser;
+  }
+
+}
