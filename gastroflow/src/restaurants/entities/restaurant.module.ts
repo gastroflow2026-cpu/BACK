9a6f9d7ad53475eaff999dbbd4e83953f92358cf
Hiram Carreño <143430604+Abiff11@users.noreply.github.com>
2026-04-10 22:59:32 -0600

    feat/fix-restaurant-theme-metadata

--- gastroflow/src/restaurants/entities/restaurant.module.ts
+++ gastroflow/src/restaurants/entities/restaurant.module.ts
@@ -3,9 +3,10 @@ import { Restaurant } from "./restaurant.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RestaurantController } from "./restaurant.controller";
 import { RestaurantService } from "./restaurant.service";
+import { RestaurantTheme } from "../../restaurant-theme/entities/restaurant-theme.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Restaurant])],
+    imports: [TypeOrmModule.forFeature([Restaurant, RestaurantTheme])],
     exports: [TypeOrmModule],
     controllers: [RestaurantController],
     providers: [RestaurantService]
