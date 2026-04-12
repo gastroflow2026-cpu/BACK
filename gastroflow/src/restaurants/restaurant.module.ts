eb7037120cb5e037cb0dc777e66c10d14073b53d
Nicolas Malvano <malvanonicolas@gmail.com>
2026-04-12 14:18:32 -0300

    Fix/Borrados archivos duplicados en restaurants

--- gastroflow/src/restaurants/restaurant.module.ts
+++ gastroflow/src/restaurants/restaurant.module.ts
@@ -3,9 +3,10 @@ import { RestaurantController } from './restaurant.controller';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantTheme } from '../restaurant-theme/entities/restaurant-theme.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant])],
+  imports: [TypeOrmModule.forFeature([Restaurant, RestaurantTheme])],
   controllers: [RestaurantController],
   providers: [RestaurantService],
   exports: [RestaurantService],
